21cfb42338a8055e41f26feb0cb61ff38c929a1c
Maratatasan <marats.stelihs@gmail.com>
2021-10-28 13:55:01 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import './App.css';
+import { useEffect, useState } from 'react';
+
+// let myPromise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve('hello world')
+//   }, 5000 )
+// });
+
+// myPromise
+//   .then(res => console.log(res))
+//   .catch(err => console.log(err));
+
+function App() {
+
+  const [message, setMessage] = useState('penis')
+
+  useEffect(() => {
+    const myPromise = new Promise((res, rej)=> {
+      setTimeout(()=>{
+        res('Cau Pasule!')
+      }, 100)
+    })
+
+    myPromise.then(res => {setMessage(res)})
+    // fetch('http://localhost:8000/')
+    //   .then((res) => {
+    //     console.log(res)
+    //   })
+
+  }, [])
+
+
+  return (
+    <div className="App">
+      <h1>{message}</h1>
+      <input type="text" placeholder={'penis'}></input>
+    </div>
+  );
+}
+
+export default App;
