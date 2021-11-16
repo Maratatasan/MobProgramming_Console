1259a2aa92a4af881e9c12cb8eb9af7eaa5c41fa
bamfard@gmail.com <^,+jTk'(wiq"ybh&Ga5[y>
2021-11-16 13:56:10 +0000

    showing games from the server

--- src/App.js
+++ src/App.js
@@ -1,42 +1,66 @@
 import './App.css';
-import { useEffect, useState } from 'react';
-
-// let myPromise = new Promise((resolve, reject) => {
-//   setTimeout(() => {
-//     resolve('hello world')
-//   }, 5000 )
-// });
-
-// myPromise
-//   .then(res => console.log(res))
-//   .catch(err => console.log(err));
+import { useEffect, useState, useRef } from 'react';
+import axios from 'axios'
+// from client, you should see a list of games
+// and select what game to want to play
+// register to the game with an input box for username
 
 function App() {
 
-  const [message, setMessage] = useState('penis')
+  //fruit loops
+
+  const [game, setGame] = useState('penis');
+
+  const [allGames, setAllGames] = useState([])
+
+  const gameInput = useRef()
+
+
+  
 
   useEffect(() => {
-
-
-    // const myPromise = new Promise((res, rej)=> {
-    //   setTimeout(()=>{
-    //     res('Cau Pasule!')
-    //   }, 100)
-    // })
-
-    // myPromise.then(res => {setMessage(res)})
-    fetch('http://localhost:8000/')
-      .then((res) => {
-        console.log(res)
-      })
-
+    axios({
+      method: 'get',
+      url: 'http://localhost:8000/games',
+    })
+      .then(function (response) {
+        setAllGames(response.data)
+      });
   }, [])
 
+  const handleButton = () => {
+    console.log('button clicked',gameInput)
+    axios.post(
+      'http://localhost:8000/addGame',
+      {
+        newGame: gameInput.current.value
+      })
+      .then(function (response) {
+        
+        setAllGames(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  const registerUser = (game) => {
+
+    // const userName = 
+  }
 
   return (
     <div className="App">
-      <h1>{message}</h1>
-      <input type="text" placeholder={'penis'}></input>
+      <h1>Add Games</h1>
+      <button onClick={handleButton}>add</button>
+      <input ref={gameInput} type="text" placeholder="games"></input>
+
+      <div> all games:</div>
+      <ul>{allGames.map(game=>(<div>
+      <li key={game}>{game}</li>
+      <button onClick={ e => registerUser(game)}>register</button>
+      <input id={`${game}-username`} type="text" placeholder="username..."></input>
+      </div>))}</ul>
     </div>
   );
 }
