f4d738dfe808dca45c2945ba9bb91a948c83cd71
icedjello <bamfard@gmail.com>
2022-03-03 15:07:27 +0000

    Re-understood what's going on with FE. We need to CODE TO THE INTERFACE.

--- src/App.js
+++ src/App.js
@@ -1,67 +1,54 @@
 import './App.css';
-import { useEffect, useState, useRef } from 'react';
+import {useEffect, useState, useRef} from 'react';
 import axios from 'axios'
 // from client, you should see a list of games
 // and select what game to want to play
 // register to the game with an input box for username
-
 function App() {
+  const newGameInputRef = useRef()
+  const [games, setGames] = useState([])
 
-  //fruit loops
-
-  const [game, setGame] = useState('penis');
-
-  const [allGames, setAllGames] = useState([])
-
-  const gameInput = useRef()
-
-
-  
+  let userName = '';
 
   useEffect(() => {
     axios({
       method: 'get',
       url: 'http://localhost:8000/games',
     })
-      .then(function (response) {
-        setAllGames(response.data)
-      });
+        .then(response => setGames(response.data));
   }, [])
 
-  const handleButton = () => {
-    console.log('button clicked',gameInput)
+  const onAddGame = () => {
     axios.post(
-      'http://localhost:8000/addGame',
-      {
-        newGame: gameInput.current.value
-      })
-      .then(function (response) {
-        
-        setAllGames(response.data);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+        'http://localhost:8000/addGame',
+        {
+          newGame: newGameInputRef.current.value
+        })
+        .then(response => setGames(response.data))
+        .catch(error => console.log(error));
   }
 
   const registerUser = (game) => {
-
-    // const userName = 
+    console.log(game, userName);
+    // axios.post('')
+    // const userName =
   }
 
   return (
-    <div className="App">
-      <h1>Add Games</h1>
-      <button onClick={handleButton}>add</button>
-      <input ref={gameInput} type="text" placeholder="games"></input>
-
-      <div> all games:</div>
-      <ul>{allGames.map(game=>(<div>
-      <li key={game}>{game}</li>
-      <button onClick={ e => registerUser(game)}>register</button>
-      <input id={`${game}-username`} type="text" placeholder="username..."></input>
-      </div>))}</ul>
-    </div>
+      <div className="App">
+        <h1>Add Games</h1>
+        <button onClick={onAddGame}>add</button>
+        <input ref={newGameInputRef} type="text" placeholder="games"/>
+        <div> all games:</div>
+        <ul>{
+          games.map(game => (
+              <div key={game}>
+                <li>{game}</li>
+                <button onClick={(e) => registerUser(game)}>register</button>
+                <input id={`${game}-username`} type="text"  onChange={(e)=> {userName = e.target.value}} placeholder="username..."/>
+              </div>))
+        }</ul>
+      </div>
   );
 }
 
